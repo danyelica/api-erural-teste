b2ef65c0f8369a1d9a3431313cd92408b72fc9ed
Danyélica <danydayane67@gmail.com>
2022-10-19 17:42:27 -0300

    conexão no banco de dados

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const knex = require("./connection/connection");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ mensagem: "api funcionando" });
+app.get("/", async (req, res) => {
+  try {
+    const salas = await knex("salas");
+    return res.status(200).json(salas);
+  } catch (error) {
+    res.json({ mensagem: "Erro interno no servidor" });
+  }
 });
 
 app.listen(process.env.PORT || 3000);
