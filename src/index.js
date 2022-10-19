b7fc896282e812a7eb9c139ede8426ddbe5be33c
Danyélica <danydayane67@gmail.com>
2022-10-19 20:21:52 -0300

    erro corrigido e comentários apagados

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-/*const {
-  listingRooms,
-  creatingRoom,
-  updatingRoom,
-} = require("./controllers/salas");
-const { creatingUser } = require("./controllers/participantes");*/
 const router = require("./router");
 
 const app = express();
@@ -15,12 +9,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
-/*app.get("/salas", listingRooms);
-app.get("/salas/:id", listingUsersInThisRoom);
-app.post("/salas", creatingRoom);
-app.put("/salas/:id", updatingRoom);
-
-app.post("/participantes", creatingUser);*/
 
 app.listen(process.env.PORT || 3000);
