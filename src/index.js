0baef7fbd3c8c20a623e50d5626ebc6173a42e6b
Danyélica <danydayane67@gmail.com>
2022-10-19 16:57:41 -0300

    projeto configurado

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ mensagem: "api funcionando" });
+});
+
+app.listen(process.env.PORT || 3000);
