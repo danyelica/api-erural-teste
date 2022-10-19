0b7c5f40a9e47410f55ee8f4b4f2a60518624497
Danyélica <danydayane67@gmail.com>
2022-10-19 20:09:32 -0300

    tentando outra coisa

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const {
+/*const {
   listingRooms,
   creatingRoom,
   updatingRoom,
 } = require("./controllers/salas");
-const { creatingUser } = require("./controllers/participantes");
+const { creatingUser } = require("./controllers/participantes");*/
+const router = require("./router");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/salas", listingRooms);
+app.use(router);
+
+/*app.get("/salas", listingRooms);
 app.get("/salas/:id", listingUsersInThisRoom);
 app.post("/salas", creatingRoom);
 app.put("/salas/:id", updatingRoom);
 
-app.post("/participantes", creatingUser);
+app.post("/participantes", creatingUser);*/
 
 app.listen(process.env.PORT || 3000);
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {
   listingRooms,
+  listingUsersInThisRoom,
   creatingRoom,
   updatingRoom,
 } = require("./controllers/salas");
