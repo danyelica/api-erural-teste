e9d9d7a67d85ff4246bcdcf251c1d4a6cb5c301e
Danyélica <danydayane67@gmail.com>
2022-10-19 18:03:11 -0300

    erro corrigido

--- src/controllers/salas.js
+++ src/controllers/salas.js
@@ -2,7 +2,7 @@ const knex = require("../connection/connection");
 
 const listandoSalas = async (req, res) => {
   try {
-    const salas = knex("salas");
+    const salas = await knex("salas");
     res.status(201).json(salas);
   } catch (error) {
     res.json({ mensagem: "Erro interno no servidor", erro: error.message });
@@ -12,7 +12,7 @@ const listandoSalas = async (req, res) => {
 const criandoSala = async (req, res) => {
   const { video } = req.body;
   try {
-    const sala = knex("salas").insert(video).returning(["id", "video"]);
+    const sala = await knex("salas").insert(video).returning(["id", "video"]);
     res.status(201).json(sala);
   } catch (error) {
     res.json({ mensagem: "Erro interno no servidor", erro: error.message });
