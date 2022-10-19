4c0036d4c8b5362da0eb5fb76b524c379228fded
Danyélica <danydayane67@gmail.com>
2022-10-19 20:06:18 -0300

    tentando arrumar o erro

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./router");
 
+const {
+  listingRooms,
+  creatingRoom,
+  updatingRoom,
+} = require("./controllers/salas");
+const { creatingUser } = require("./controllers/participantes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.get("/salas", listingRooms);
+app.get("/salas/:id", listingUsersInThisRoom);
+app.post("/salas", creatingRoom);
+app.put("/salas/:id", updatingRoom);
+
+app.post("/participantes", creatingUser);
 
 app.listen(process.env.PORT || 3000);
